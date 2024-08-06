f15701c1a9a55e6a0e1ab55b481358b7ee31f878
harik8 <khprasaad@gmail.com>
2024-08-06 02:48:18 +0200

    D-290724: Enable grafana loki

--- workloads/acb/regional/eks/locals.tf
+++
@@ -1,9 +0,0 @@
-locals {
-  nlb_public_tags = merge(
-    module.tag.default_tags,
-    {
-      Name        = join("-", [module.tag.default_tags["Prefix"], "nlb-public"]),
-      Description = "The public NLB."
-    }
-  )
-}
